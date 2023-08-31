d1dc4a9b1a12811b4b87ad56fc500f0a13370776
Scott Consaul Atkinson <scabecks@gmail.com>
2023-08-31 14:39:44 +1000

    Attempted fixes

--- docs/conf.py
+++ docs/conf.py
@@ -17,8 +17,8 @@
 
 # -- Project information -----------------------------------------------------
 
-project = "Basic Sphinx Example Project"
-copyright = "2022, Read the Docs core team"
+project = "ELSA Sphinx Example Project"
+copyright = "2023, Read the Docs core team"
 author = "Read the Docs core team"
 
 
@@ -55,7 +55,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+# html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
--- docs/index.rst
+++ docs/index.rst
@@ -1,15 +1,11 @@
 .. include:: ../README.rst
 
-Welcome to Lumache's documentation!
+Welcome to ELSA's documentation!
 ===================================
 
-**Lumache** (/lu'make/) is a Python library for cooks and food lovers
-that creates recipes mixing random ingredients.
-It pulls data from the `Open Food Facts database <https://world.openfoodfacts.org/>`_
-and offers a *simple* and *intuitive* API.
+**ELSA** is a work in progress
 
-Check out the :doc:`usage` section for further information, including
-how to :ref:`installation` the project.
+Check out the :doc:`usage` section for further information
 
 .. note::
 
